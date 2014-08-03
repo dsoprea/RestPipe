b588bd54cd7d5e3accf4e4e7bd5106e5a9dccdd0
Dustin Oprea <myselfasunder@gmail.com>
2014-08-03 10:14:45 -0400

    Version bump. Changed log name.

--- rpipe/resources/data/gunicorn_conf_client_prod.py
+++ rpipe/resources/data/gunicorn_conf_client_prod.py
@@ -9,6 +9,6 @@ daemon = 'true'
 bind = 'unix:/tmp/rpclient.gunicorn.sock'
 
 _LOG_PATH = '/var/log'
-errorlog = os.path.join(_LOG_PATH, 'app_rpipe.log')
+errorlog = os.path.join(_LOG_PATH, 'restpipe.log')
 loglevel = 'info'
 worker_class = 'gevent'
