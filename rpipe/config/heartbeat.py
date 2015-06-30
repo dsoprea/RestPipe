dc461be7f5eaa4c426834ae27e10c170728e1008
Dustin Oprea <myselfasunder@gmail.com>
2015-06-30 01:57:38 -0400

    Increased timeout to 10s (from 3s).

--- rpipe/config/heartbeat.py
+++ rpipe/config/heartbeat.py
@@ -1,4 +1,4 @@
 import os
 
 HEARTBEAT_INTERVAL_S = int(os.environ.get('RP_CLIENT_HEARTBEAT_INTERVAL_S', '5'))
-HEARTBEAT_TIMEOUT_S = int(os.environ.get('RP_CLIENT_HEARTBEAT_TIMEOUT_S', '3'))
+HEARTBEAT_TIMEOUT_S = int(os.environ.get('RP_CLIENT_HEARTBEAT_TIMEOUT_S', '10'))
